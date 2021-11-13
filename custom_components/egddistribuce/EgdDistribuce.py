9dddedab12e92d693f8b50d642d9929b580560a1
Antrac1t <33174552+Antrac1t@users.noreply.github.com>
2021-11-13 22:21:32 +0100

    Add files via upload

---
+++ custom_components/egddistribuce/EgdDistribuce.py
@@ -0,0 +1,110 @@
+__version__ = "0.1"
+
+import logging
+import json
+from urllib.request import urlopen
+from . import downloader
+import voluptuous as vol
+import datetime
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.util import Throttle
+
+import requests
+from lxml import html, etree
+
+MIN_TIME_BETWEEN_SCANS = timedelta(seconds=300)
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "egddistribuce"
+CONF_PSC = "psc"
+CONF_A = "code_a"
+CONF_B = "code_b"
+CONF_DP = "code_dp"
+CONF_NAME = "name"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_PSC): cv.string,
+        vol.Required(CONF_A): cv.string,
+        vol.Required(CONF_B): cv.string, 
+        vol.Required(CONF_DP): cv.string,
+        vol.Required(CONF_NAME): cv.string,
+    }
+)
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    name = config.get(CONF_NAME)
+    psc = config.get(CONF_PSC)
+    codeA = config.get(CONF_A)
+    codeB = config.get(CONF_B)
+    codeDP = config.get(CONF_DP)
+
+    ents = []
+    ents.append(EgdDistribuce(name,psc,codeA,codeB,codeDP))
+    add_entities(ents)
+
+class EgdDistribuce(BinarySensorEntity):
+    def __init__(self, name, psc, codeA, codeB, codeDP):
+        """Initialize the sensor."""
+        self._name = name
+        self.psc = psc
+        self.codeA = codeA
+        self.codeB = codeB
+        self.codeDP = codeDP
+        self.responseRegionJson = "[]"
+        self.responseHDOJson ="[]"
+        self.Region = ""
+        self.update()
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def icon(self):
+        return "mdi:power"
+
+    @property
+    def is_on(self):
+        return downloader.isHdo(self.codeA,self.codeB,self.codeDP)
+
+   @property
+   def device_state_attributes(self):
+       attributes = {}
+       attributes['response_json'] = self.responseHDOJson
+       return attributes
+
+    @property
+    def should_poll(self):
+        return True
+
+    @property
+    def available(self):
+        return self.last_update_success
+
+    @property
+    def device_class(self):
+        return ''
+
+    @Throttle(MIN_TIME_BETWEEN_SCANS)
+    def update(self):
+        # PSC = "67168"
+        # CODE_A = "1"
+        # CODE_B = "8"
+        # CODE_DP = "05"
+
+        responseRegion = requests.get(downloader.getRegion())
+        if responseRegion.status_code == 200:
+            self.responseRegionJson = responseRegion.json()
+            self.Region=parseRegion(self.responseRegionJson,self.psc)
+            responseHDO = requests.get(downloader.getHDO())
+            if responseHDO.status_code == 200:
+                self.last_update_success = True
+                self.responseHDOJson = responseHDO.json()
+            #regionHDO=parseHDO(responseHDOJson,regionResult,"1","8","05")
+
+        else:
+            self.last_update_success = False
+
---
+++ custom_components/egddistribuce/downloader.py
@@ -0,0 +1,59 @@
+import json
+import datetime
+from urllib.request import urlopen
+
+HDO_Date_Od=[]
+HDO_Date_Do=[]
+
+HDO_Cas_Od = []
+HDO_Cas_Do = []
+
+def getRegion():
+    return "https://hdo.distribuce24.cz/region"
+
+def getHDO():
+    return "https://hdo.distribuce24.cz/casy"
+    
+def parseRegion(jsonRegion,psc):
+    #input_region_dict = json.load(regionFile )
+    output_region_dict = [x for x in jsonRegion if x['PSC'] == psc]
+    unique_region_list = []
+    for itemRegion in output_region_dict:  
+        if itemRegion['Region'] not in unique_region_list:
+            unique_region_list.append(itemRegion['Region'])
+    return unique_region_list[0]
+
+def parseHDO(jsonHDO,HDORegion,HDO_A,HDO_B,HDO_DP):
+    output_hdo_dict = [x for x in jsonHDO if x['A'] == HDO_A and x['B'] == HDO_B and x['DP'] == HDO_DP and x['region'] == HDORegion]
+
+    dateNow = datetime.datetime.now()
+
+    for itemData in output_hdo_dict: 
+        HDOStatus=False
+        str_date_time_od = itemData['od']['rok'] + "-" + itemData['od']['mesic'] + "-" + itemData['od']['den']
+        str_date_time_do = itemData['do']['rok'] + "-" + itemData['do']['mesic'] + "-" + itemData['do']['den']
+        date_time_od_obj = datetime.datetime.strptime(str_date_time_od, '%Y-%m-%d')
+        date_time_do_obj = datetime.datetime.strptime(str_date_time_do, '%Y-%m-%d')
+
+        if date_time_od_obj <= dateNow <= date_time_do_obj:
+            HDO_Date_Od=(str_date_time_od )   
+            HDO_Date_Do=(str_date_time_do )
+            for itemDataSazby in itemData['sazby']:
+                HDO_Sazba = (itemDataSazby['sazba'])
+                for itemDataDny in itemDataSazby['dny']:
+                    if dateNow.isoweekday() == itemDataDny['denVTydnu']:
+#                       print(itemDataDny['denVTydnu'])
+                        for itemDataCasy in itemDataDny['casy']:
+                            #print(itemDataCasy['od'])
+                            #print(itemDataCasy['do'])
+                            HDO_Cas_Od.append(itemDataCasy['od'])
+                            HDO_Cas_Do.append(itemDataCasy['do'])
+
+            for x in range(len(HDO_Cas_Od)):
+                HDD_Date_od_obj = datetime.datetime.strptime(HDO_Cas_Od[x], '%H:%M:%S')
+                HDD_Date_do_obj = datetime.datetime.strptime(HDO_Cas_Do[x], '%H:%M:%S')
+                timeNow = datetime.datetime.strptime(dateNow.strftime('%H:%M:%S'), '%H:%M:%S')
+                if HDD_Date_od_obj <= timeNow <= HDD_Date_do_obj:
+                    HDOStatus=True
+                
+    return(HDOStatus)
