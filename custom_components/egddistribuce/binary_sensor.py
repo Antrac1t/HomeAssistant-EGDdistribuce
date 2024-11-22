6fd86dd8059d064cf0504c79acef535a66308a3c
Antrac1t <33174552+Antrac1t@users.noreply.github.com>
2024-11-22 13:43:06 +0100

    Update binary_sensor.py
    Adding remaining time attribute

--- custom_components/egddistribuce/binary_sensor.py
+++ custom_components/egddistribuce/binary_sensor.py
@@ -1,9 +1,9 @@
-__version__ = "0.5"
+__version__ = "0.3"
 
 import logging
 from . import downloader
 import voluptuous as vol
-from datetime import timedelta
+from datetime import datetime, timedelta
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.binary_sensor import BinarySensorEntity
@@ -35,6 +35,43 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     }
 )
 
+class HDOManager:
+    def __init__(self, HDO_Cas_Od, HDO_Cas_Do):
+        self.HDO_Cas_Od = HDO_Cas_Od
+        self.HDO_Cas_Do = HDO_Cas_Do
+
+    def get_remaining_time(self):
+        current_time = datetime.now().time()  # Pouze čas
+        schedule = []
+        print(current_time)
+        for start_time_str, end_time_str in zip(self.HDO_Cas_Od, self.HDO_Cas_Do):
+            try:
+                start_time = datetime.strptime(start_time_str, "%H:%M:%S").time()
+                end_time = datetime.strptime(end_time_str, "%H:%M:%S").time()
+            except ValueError as e:
+                continue
+
+            if current_time < start_time:
+                schedule.append((start_time, end_time))
+            elif start_time <= current_time < end_time:
+                schedule.append((current_time, end_time))
+
+        if not schedule:
+            return "Není dostupný žádný další čas VT/NT"
+
+        first_start, first_end = schedule[0]
+
+        if current_time < first_start:
+            remaining_time = datetime.combine(datetime.today(), first_start) - datetime.combine(datetime.today(), current_time)
+        else:
+            remaining_time = datetime.combine(datetime.today(), first_end) - datetime.combine(datetime.today(), current_time)
+
+        remaining_hours = remaining_time.seconds // 3600
+        remaining_minutes = (remaining_time.seconds % 3600) // 60
+
+        return f"{remaining_hours}h {remaining_minutes}m"
+
+
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     name = config.get(CONF_NAME)
@@ -45,8 +82,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     priceNT = config.get(CONF_PRICE_NT)
     priceVT = config.get(CONF_PRICE_VT)
 
-    add_entities(
-        [EgdDistribuce(name, psc, codeA, codeB, codeDP, priceNT, priceVT)])
+    add_entities([EgdDistribuce(name, psc, codeA, codeB, codeDP, priceNT, priceVT)])
 
 
 class EgdDistribuce(BinarySensorEntity):
@@ -65,16 +101,10 @@ class EgdDistribuce(BinarySensorEntity):
         self.HDO_Cas_Do = []
         self.update()
         self._attributes = {}
+        self.hdo_manager = HDOManager(self.HDO_Cas_Od, self.HDO_Cas_Do)
 
-        if priceNT is not None:
-            self.priceNT = priceNT
-        else:
-            self.priceNT = 0
-
-        if priceVT is not None:
-            self.priceVT = priceVT
-        else:
-            self.priceVT = 0
+        self.priceNT = priceNT if priceNT is not None else 0
+        self.priceVT = priceVT if priceVT is not None else 0
 
     @property
     def name(self):
@@ -82,16 +112,12 @@ class EgdDistribuce(BinarySensorEntity):
 
     @property
     def icon(self):
-        if self.status == True:
-            return "mdi:transmission-tower"
-        else:
-            return "mdi:power-off"
+        return "mdi:transmission-tower" if self.status else "mdi:power-off"
 
     @property
     def is_on(self):
         self.status, self.HDO_Cas_Od, self.HDO_Cas_Do, self.HDO_HOURLY, self.price = downloader.parse_HDO(
             self, self.responseHDOJson, self.region, self.codeA, self.codeB, self.codeDP, self.priceNT, self.priceVT)
-
         return self.status
 
     @property
@@ -101,7 +127,8 @@ class EgdDistribuce(BinarySensorEntity):
             'HDO Times': self.get_times(),
             'HDO_Cas_Od': self.HDO_Cas_Od,
             'HDO_Cas_Do': self.HDO_Cas_Do,
-            'current_price': self.price
+            'current_price': self.price,
+            'remaining_time': self.hdo_manager.get_remaining_time()
         }
 
     @property
@@ -114,15 +141,16 @@ class EgdDistribuce(BinarySensorEntity):
 
     @property
     def device_class(self):
-        return ''
+        return 'monetary'
+
+    @property
+    def unit_of_measurement(self):
+        return 'Kč/kWh'
 
     def get_times(self):
-        i = 0
         timeReport = []
-        for n in self.HDO_Cas_Od:
-            timeReport.append(
-                '{}'.format(n).replace(':00', '') + ' - ' + self.HDO_Cas_Do[i].replace(':00', ''))
-            i += 1
+        for i, start_time in enumerate(self.HDO_Cas_Od):
+            timeReport.append(f'{start_time.replace(":00", "")} - {self.HDO_Cas_Do[i].replace(":00", "")}')
         return ', '.join(timeReport)
 
     @Throttle(MIN_TIME_BETWEEN_SCANS)
@@ -130,11 +158,11 @@ class EgdDistribuce(BinarySensorEntity):
         responseRegion = requests.get(downloader.get_region(), verify=True)
         if responseRegion.status_code == 200:
             self.responseRegionJson = responseRegion.json()
-            self.region = downloader.parse_region(
-                self.responseRegionJson, self.psc)
+            self.region = downloader.parse_region(self.responseRegionJson, self.psc)
             responseHDO = requests.get(downloader.get_HDO(), verify=True)
             if responseHDO.status_code == 200:
                 self.responseHDOJson = responseHDO.json()
+                self.hdo_manager = HDOManager(self.HDO_Cas_Od, self.HDO_Cas_Do)
                 self.last_update_success = True
         else:
             self.last_update_success = False
