038df558dfcf0d916c915e8901a5d6858c08cce0
Antrac1t <33174552+Antrac1t@users.noreply.github.com>
2025-08-25 08:12:39 +0200

    Update downloader.py
    update for next day calculation

--- custom_components/egddistribuce/downloader.py
+++ custom_components/egddistribuce/downloader.py
@@ -1,23 +1,27 @@
+# downloader.py
+
 from datetime import datetime, timedelta, time
-from zoneinfo import ZoneInfo
-from typing import Dict
 import holidays
+from typing import Dict
 import logging
 
+
+_LOGGER = logging.getLogger(__name__)
 
 def get_region():
     return "https://hdo.distribuce24.cz/region"
 
-
 def get_HDO():
     return "https://hdo.distribuce24.cz/casy"
-
 
 def parse_region(jsonRegion, psc):
     if psc == "smart":
         region = "X"
     else:
         output_region_dict = [x for x in jsonRegion if x['PSC'] == psc]
+        if not output_region_dict:
+            _LOGGER.warning(f"Region not found for PSC: {psc}")
+            return None
         unique_region_list = []
         for item_region in output_region_dict:
             if item_region['Region'] not in unique_region_list:
@@ -25,8 +29,7 @@ def parse_region(jsonRegion, psc):
         region = unique_region_list[0]
     return region
 
-
-def parse_HDO(self, jsonHDO, HDORegion, HDO_A, HDO_B, HDO_DP, HDO_priceNT, HDO_priceVT):
+def parse_HDO(jsonHDO, HDORegion, HDO_A, HDO_B, HDO_DP, HDO_priceNT, HDO_priceVT):
     HDO_Cas_Od = []
     HDO_Cas_Do = []
     HDO_Cas_Od_zitra = []
@@ -34,125 +37,91 @@ def parse_HDO(self, jsonHDO, HDORegion, HDO_A, HDO_B, HDO_DP, HDO_priceNT, HDO_p
     HDO_HOURLY: Dict[datetime, float] = {}
     HDO_Status = False
     price = HDO_priceVT
-    zoneinfo = ZoneInfo(self.hass.config.time_zone)
+
+    if HDORegion is None:
+        _LOGGER.error("Region is None. Cannot parse HDO data.")
+        return False, [], [], {}, HDO_priceVT
 
     if HDORegion == "X":
         output_hdo_dict = [x for x in jsonHDO if x['kodHdo_A'] == HDO_A]
     else:
         output_hdo_dict = [x for x in jsonHDO if x['A'] == HDO_A and x['B'] == HDO_B and (
-            x['DP'] == HDO_DP or x['DP'] == '0' + HDO_DP) and x['region'] == HDORegion]
+                x['DP'] == HDO_DP or x['DP'] == '0' + HDO_DP) and x['region'] == HDORegion]
 
     date_now = datetime.now().date()
     date_tomorrow = datetime.now().date() + timedelta(days=1)
 
     def get_holiday(date):
-        cz_holidays = holidays.CZ()  # this is a dict
+        cz_holidays = holidays.CZ()
         return (date in cz_holidays)
 
     for itemData in output_hdo_dict:
         current_year = datetime.now().year
         od_year = int(itemData['od']['rok'])
 
-        # Pokud je rok 'od' menší než aktuální rok, použij nižší rok pro zpracování
-        if od_year < current_year:
+        if od_year < current_year and int(itemData['od']['mesic']) > datetime.now().month:
             current_year = current_year - 1
-        else:
-            current_year = current_year
+        
+        do_year = int(itemData['do']['rok'])
+        if do_year < od_year:
+             do_year = do_year + 1
 
-        if int(itemData['od']['rok']) == 9999:
-            # year 9999 could mean this year, next year, maybe even the previous year
-
-            if int(itemData['od']['mesic']) > int(itemData['do']['mesic']):
-                if int(itemData['do']['mesic']) >= datetime.now().month:
-                    odYear = current_year - 1
-                    doYear = current_year
-                else:
-                    odYear = current_year
-                    doYear = current_year + 1
-            else:
-                odYear = current_year
-                doYear = current_year
-        else:
-            odYear = itemData['od']['rok']
-            doYear = itemData['do']['rok']
-
-        str_date_time_od = f"{odYear}-{itemData['od']['mesic']}-{itemData['od']['den']}"
-        str_date_time_do = f"{doYear}-{itemData['do']['mesic']}-{itemData['do']['den']}"
-        date_time_od_obj = datetime.strptime(
-            str_date_time_od, '%Y-%m-%d')
-        date_time_do_obj = datetime.strptime(
-            str_date_time_do, '%Y-%m-%d')
+        str_date_time_od = f"{current_year}-{itemData['od']['mesic']}-{itemData['od']['den']}"
+        str_date_time_do = f"{do_year}-{itemData['do']['mesic']}-{itemData['do']['den']}"
+        
+        try:
+            date_time_od_obj = datetime.strptime(str_date_time_od, '%Y-%m-%d')
+            date_time_do_obj = datetime.strptime(str_date_time_do, '%Y-%m-%d')
+        except ValueError as e:
+            _LOGGER.warning(f"Could not parse date {str_date_time_od} or {str_date_time_do}: {e}")
+            continue
 
         if date_time_od_obj.date() <= date_now <= date_time_do_obj.date():
             for tarifs in itemData['sazby']:
                 for days in tarifs['dny']:
-                    if get_holiday(date_now) == True:
-                        if 7 == days['denVTydnu']:
-                            for times in days['casy']:
-                                HDO_Cas_Od.append(times['od'])
-                                HDO_Cas_Do.append(times['do'])
-                    else:
-                        if date_now.isoweekday() == days['denVTydnu']:
-                            for times in days['casy']:
-                                HDO_Cas_Od.append(times['od'])
-                                HDO_Cas_Do.append(times['do'])
+                    is_holiday = get_holiday(date_now)
+                    if (is_holiday and days['denVTydnu'] == 7) or (not is_holiday and date_now.isoweekday() == days['denVTydnu']):
+                        for times in days['casy']:
+                            HDO_Cas_Od.append(times['od'])
+                            HDO_Cas_Do.append(times['do'])
+                    
+                    is_holiday_tomorrow = get_holiday(date_tomorrow)
+                    if (is_holiday_tomorrow and days['denVTydnu'] == 7) or (not is_holiday_tomorrow and date_tomorrow.isoweekday() == days['denVTydnu']):
+                        for times in days['casy']:
+                            HDO_Cas_Od_zitra.append(times['od'])
+                            HDO_Cas_Do_zitra.append(times['do'])
+    
+    def get_status(time_to_check: datetime, od: list, do: list):
+        result = False
+        compare_time = datetime.strptime(time_to_check.strftime('%H:%M:%S'), '%H:%M:%S')
 
-                    if get_holiday(date_tomorrow) == True:
-                        if 7 == days['denVTydnu']:
-                            for times in days['casy']:
-                                HDO_Cas_Od_zitra.append(times['od'])
-                                HDO_Cas_Do_zitra.append(times['do'])
-                    else:
-                        if date_tomorrow.isoweekday() == days['denVTydnu']:
-                            for times in days['casy']:
-                                HDO_Cas_Od_zitra.append(times['od'])
-                                HDO_Cas_Do_zitra.append(times['do'])
+        for x in range(len(od)):
+            HDD_Date_od_obj = datetime.strptime(od[x], '%H:%M:%S')
+            HDD_Date_do_obj = datetime.strptime(do[x], '%H:%M:%S')
 
-            def get_status(time: datetime, od: list, do: list):
-                result = False
-                compare_time = datetime.strptime(
-                    time.strftime('%H:%M:%S'), '%H:%M:%S')
+            if do[x].endswith(':59:00'):
+                HDD_Date_do_obj = HDD_Date_do_obj + timedelta(minutes=1)
+            
+            if HDD_Date_od_obj <= compare_time < HDD_Date_do_obj:
+                result = True
+        return result
 
-                for x in range(len(od)):
-                    HDD_Date_od_obj = datetime.strptime(
-                        od[x], '%H:%M:%S')
-                    HDD_Date_do_obj = datetime.strptime(
-                        do[x], '%H:%M:%S')
+    HDO_Status = get_status(datetime.now().replace(second=0, microsecond=0), HDO_Cas_Od, HDO_Cas_Do)
+    if HDO_Status:
+        price = HDO_priceNT
 
-                    if do[x].endswith(':59:00'):
-                        HDD_Date_do_obj = HDD_Date_do_obj+timedelta(minutes=1)
+    for x in range(0, 24*4):
+        date = datetime.combine(date_now, time=time(hour=0)) + timedelta(minutes=x*15)
+        if get_status(date, HDO_Cas_Od, HDO_Cas_Do):
+            HDO_HOURLY[date] = float(HDO_priceNT)
+        else:
+            HDO_HOURLY[date] = float(HDO_priceVT)
 
-                    if HDD_Date_od_obj <= compare_time < HDD_Date_do_obj:
-                        result = True
-                return result
-
-            HDO_Status = get_status(
-                datetime.now().replace(second=0, microsecond=0), HDO_Cas_Od, HDO_Cas_Do)
-            if HDO_Status == True:
-                price = HDO_priceNT
-
-            for x in range(0, (24*4)):
-                date = datetime.combine(date_now, time=time(
-                    hour=0)) + timedelta(minutes=x*15)
-
-                dateIso = (datetime.combine(date_now, time=time(
-                    hour=0), tzinfo=zoneinfo) + timedelta(minutes=x*15))
-
-                if get_status(date, HDO_Cas_Od, HDO_Cas_Do):
-                    HDO_HOURLY[dateIso] = float(HDO_priceNT)
-                else:
-                    HDO_HOURLY[dateIso] = float(HDO_priceVT)
-
-            for x in range(0, (24*4)):
-                date = datetime.combine(date_tomorrow, time=time(
-                    hour=0)) + timedelta(minutes=x*15)
-
-                dateIso = (datetime.combine(date_tomorrow, time=time(
-                    hour=0), tzinfo=zoneinfo) + timedelta(minutes=x*15))
-
-                if get_status(date, HDO_Cas_Od_zitra, HDO_Cas_Do_zitra):
-                    HDO_HOURLY[dateIso] = float(HDO_priceNT)
-                else:
-                    HDO_HOURLY[dateIso] = float(HDO_priceVT)
-
-    return HDO_Status, HDO_Cas_Od, HDO_Cas_Do, HDO_HOURLY, price
+    for x in range(0, 24*4):
+        date = datetime.combine(date_tomorrow, time=time(hour=0)) + timedelta(minutes=x*15)
+        if get_status(date, HDO_Cas_Od_zitra, HDO_Cas_Do_zitra):
+            HDO_HOURLY[date] = float(HDO_priceNT)
+        else:
+            HDO_HOURLY[date] = float(HDO_priceVT)
+    
+    return HDO_Status, HDO_Cas_Od, HDO_Cas_Do, HDO_HOURLY, price, HDO_Cas_Od_zitra, HDO_Cas_Do_zitra
